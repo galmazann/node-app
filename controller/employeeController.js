16061f774c392aec07a8340b59b89cc80a254a31
Gonzalo Almazan <galmazan@stu.southtexascollege.edu>
2024-11-10 19:05:17 -0600

    fetch employees working

--- controller/employeeController.js
+++ controller/employeeController.js
@@ -28,8 +28,15 @@ res.status(500).json({error: "Internal Server Error"})
 
 export const fetch = async(req, res) => {
     try {
-    res.json("Hello World")
+        //res.json("Hello World")
+        const employees = await Employee.find()
+        if(employees.length === 0) {
+            return res.status(404).json({message: "Employees not found!"})
+        
+        }
+        
+        res.status(200).json(employees)
     } catch (error) {
-    }
     res.status(500).json({error: "Internal Server Error"})
+    }
 }
